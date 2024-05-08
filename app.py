55dcba2460e3a6e7dbd5ab52a16bfd304505a947
emiivo <emilija.ivoskaite@gmail.com>
2024-05-08 10:21:14 +0300

    Edited function get_resident_contacts

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def get_all_contacts_route():
 def get_resident_contacts(resident_id):
     resident = hotel.get_resident_by_id(resident_id)
     if resident:
-        resident_contacts = get_contacts(resident.id)
+        resident_contacts = get_contacts(resident_id)
         
         if 'error' in resident_contacts:
             if '404 Client Error' in resident_contacts['error']:
